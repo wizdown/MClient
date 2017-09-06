794c96d61adf2331872609310177edea51b3bb73
wizdown <abhishek@kapow.games>
2017-09-06 16:52:23 +0530

    Added UIElements to DetailsViewController(no data only elements)

---
+++ MClient/MovieDetailsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MovieDetailsViewController.swift
+//  MClient
+//
+//  Created by gupta.a on 06/09/17.
+//  Copyright © 2017 gupta.a. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
